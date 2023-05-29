26d2ecd427eb622c6af56c69f1c0e48ce75483e1
Reejit Biswas <reejit@github.com>
2023-05-29 17:44:58 +0530

    Hola

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,9 +1,13 @@
-if status is-interactive
-    # Commands to run in interactive sessions can go here
+set fish_greeting
+#colorscript random
+alias backup='sudo timeshift --rsync --create'
+alias update='sudo pacman -Syyu --noconfirm && yay --noconfirm'
+alias config='/usr/bin/git --git-dir=$HOME/dotfiles/ --work-tree=$HOME'
+alias ..='cd ..'
+alias ...='cd ../..'
+
+function fish_command_not_found
+  bash ~/opt/command-not-found
 end
 
-set fish_greeting 
-alias backup='sudo timeshift --create --rsync'
-alias update='sudo pacman -Syyu --noconfirm && sudo yay -Syu --noconfirm'
-colorscript random
-starship init fish | source
+
